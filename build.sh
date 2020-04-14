2278ea931d13d4a24d8fd2b1bae10f978a5ed274
lucashalbert <lhalbert@lhalbert.xyz>
2020-04-14 14:03:03 -0400

    Fix image name typo in docker push command

--- build.sh
+++ build.sh
@@ -22,14 +22,14 @@ for docker_arch in amd64 arm32v6 arm64v8; do
 	
 	# Build container
         sudo docker build -f Dockerfile.${docker_arch} -t lucashalbert/kube-plex:${docker_arch}-${kube_plex_ver} .
-        sudo docker push lucashalbert/plex:${docker_arch}-${kube_plex_ver}
+        sudo docker push lucashalbert/kube-plex:${docker_arch}-${kube_plex_ver}
     else
         # Build kube-plex
         docker run -v $(pwd)/build:/go/build -e GOOS=linux -e GOARCH=${go_arch} golang /bin/bash -c "go get -d -v -u github.com/lucashalbert/kube-plex && go build -o build/kube-plex-linux-${go_arch} github.com/lucashalbert/kube-plex"
 	
 	# Build container
         docker build -f Dockerfile.${docker_arch} -t lucashalbert/kube-plex:${docker_arch}-${kube_plex_ver} .
-        docker push lucashalbert/plex:${docker_arch}-${kube_plex_ver}
+        docker push lucashalbert/kube-plex:${docker_arch}-${kube_plex_ver}
 
         # Create and annotate arch/ver docker manifest
         DOCKER_CLI_EXPERIMENTAL=enabled docker manifest create lucashalbert/kube-plex:${docker_arch}-${kube_plex_ver} lucashalbert/kube-plex:${docker_arch}-${kube_plex_ver}
